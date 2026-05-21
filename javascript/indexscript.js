6b59af42c5d6b3b2491aa2545853c9f05b6eb184
jeyasridhar0371-wq <jeyasridhar0371@gmail.com>
2026-05-21 19:14:54 +0530

    add task is modified based on local storage

--- javascript/indexscript.js
+++ javascript/indexscript.js
@@ -5,50 +5,25 @@ let addbtn = document.getElementById("plus__button")
 let inputform = document.getElementById("input__form")
 let closebtn = document.getElementById("close__button")
 
-var tasklist = JSON.parse(localStorage.getItem("tasks")) || []
+let tasklist = JSON.parse(localStorage.getItem("tasks")) || []
 // console.log(tasklist)
-// var tasklist = []
 
-// console.log(typeof(tasklist))
-closebtn.addEventListener("click", () => {
-    
-    let object = {
-        task: "",
-        date: "",
-        category: "",
-        checked: false
-    }
-    let inputbox1 = document.getElementById("task-input")
-    let inputbox2 = document.getElementById("due-date")
-    let inputbox3 = document.getElementById("select")
-    // console.log(inputbox1.value)
-    object.task = inputbox1.value
-    object.date = inputbox2.value.toString()
-    object.category = inputbox3.value
-    tasklist.push({ ...object })
-    localStorage.setItem("tasks", JSON.stringify(tasklist))
-    // localStorage.removeItem("tasks")
-    
-});
+// JavaScript code for refreshing the page and showing the task form when the add button is clicked
+let refresh_page = () => {
+    location.reload()
+}
 
-// JavaScript code for add task 
-
+// JavaScript code for showing the task form when the add button is clicked
 addbtn.addEventListener("click", () => {
     inputform.classList.remove("hidden")
 });
 
-closebtn.addEventListener("click", () => {
-    event.preventDefault()
-    let form = document.getElementById("form__tag")
-    let inputbox1 = document.getElementById("task-input")
-    let inputbox2 = document.getElementById("due-date")
-    let inputbox3 = document.getElementById("select")
-    if (inputbox1.value === "" || inputbox2.value === "" || inputbox3.value === "") {
-        alert("Please fill out the form properly before submitting.")
-    }
-    else {
-        let noTask = document.getElementById("no__task")
-        noTask.style.display = "none"
+// JavaScript code for displaying task on the page 
+let display_card = () => {
+    if (tasklist.length > 0) {
+    let noTask = document.getElementById("no__task")
+    noTask.style.display = "none"
+    tasklist.forEach(element => {
         let div1 = document.createElement("div")
         let div2 = document.createElement("div")
         let div3 = document.createElement("div")
@@ -62,9 +37,9 @@ closebtn.addEventListener("click", () => {
         p1.classList.add("inprogress__category__p")
         p2.classList.add("inprogress__datetime")
         h1.classList.add("inprogress__content__h1")
-        h1.textContent = inputbox1.value
-        p2.textContent = inputbox2.value
-        p1.textContent = inputbox3.value
+        h1.textContent = element.task
+        p2.textContent = element.date
+        p1.textContent = element.category
         div2.appendChild(p1)
         div3.innerHTML = `<input type="checkbox">`
         div3.appendChild(h1)
@@ -73,10 +48,50 @@ closebtn.addEventListener("click", () => {
         div1.appendChild(p2)
         div1.innerHTML += `<button class="inprogress__task__button">Edit</button>`
         document.getElementById("inprogress__container").appendChild(div1)
+    });
+}
+else {
+    let noTask = document.getElementById("no__task")
+    noTask.style.display = "block"
+}
+}
+display_card()
 
-        inputform.classList.add("hidden")
-
+// Local storage code for add task
+closebtn.addEventListener("click", () => {
+    event.preventDefault()
+    let inputbox1 = document.getElementById("task-input")
+    let inputbox2 = document.getElementById("due-date")
+    let inputbox3 = document.getElementById("select")
+    if (inputbox1.value === "" || inputbox2.value === "" || inputbox3.value === "") {
+        alert("Please fill out the form properly before submitting.")
     }
-    form.reset()
+    else {
+        
+        // object literal to store the task details
+        let object = {
+            task: "",
+            date: "",
+            category: "",
+            checked: false
+        }
+        let inputbox1 = document.getElementById("task-input")
+        let inputbox2 = document.getElementById("due-date")
+        let inputbox3 = document.getElementById("select")
+        // console.log(inputbox1.value)
+        object.task = inputbox1.value
+        object.date = inputbox2.value.toString()
+        object.category = inputbox3.value
+        tasklist.push({ ...object }) // using spread operator to create a new object and push it to the tasklist array
+        localStorage.setItem("tasks", JSON.stringify(tasklist))
+        // localStorage.removeItem("tasks")
+        inputform.classList.add("hidden")
+        let form = document.getElementById("form__tag")
+        form.reset()
+        display_card()
+        refresh_page()
+    }
+
+
 });
 
