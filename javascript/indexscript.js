3f9aaec537b25cf05a38c269a8814e1d0c762c0e
jeyasridhar0371-wq <jeyasridhar0371@gmail.com>
2026-05-21 18:00:25 +0530

    Localstorage is enabled

--- javascript/indexscript.js
+++ javascript/indexscript.js
@@ -1,30 +1,61 @@
-var addbtn = document.getElementById("plus__button")
-var inputform = document.getElementById("input__form")
-var closebtn = document.getElementById("close__button")
-var inputbox1=document.getElementById("task-input")
-var inputbox2=document.getElementById("due-date")
-var inputbox3=document.getElementById("select")
 
-addbtn.addEventListener("click",()=>{
-    inputform.classList.remove("hidden")
-})
 
-closebtn.addEventListener("click",()=>{
-    event.preventDefault()
-    var form = document.getElementById("form__tag")
-    if (inputbox1.value==="" || inputbox2.value===""|| inputbox3.value===""){
-            alert("Please fill out the form properly before submitting.")
+
+let addbtn = document.getElementById("plus__button")
+let inputform = document.getElementById("input__form")
+let closebtn = document.getElementById("close__button")
+
+var tasklist = JSON.parse(localStorage.getItem("tasks")) || []
+// console.log(tasklist)
+// var tasklist = []
+
+// console.log(typeof(tasklist))
+closebtn.addEventListener("click", () => {
+    
+    let object = {
+        task: "",
+        date: "",
+        category: "",
+        checked: false
     }
-    else{
-        var noTask = document.getElementById("no__task")
+    let inputbox1 = document.getElementById("task-input")
+    let inputbox2 = document.getElementById("due-date")
+    let inputbox3 = document.getElementById("select")
+    // console.log(inputbox1.value)
+    object.task = inputbox1.value
+    object.date = inputbox2.value.toString()
+    object.category = inputbox3.value
+    tasklist.push({ ...object })
+    localStorage.setItem("tasks", JSON.stringify(tasklist))
+    // localStorage.removeItem("tasks")
+    
+});
+
+// JavaScript code for add task 
+
+addbtn.addEventListener("click", () => {
+    inputform.classList.remove("hidden")
+});
+
+closebtn.addEventListener("click", () => {
+    event.preventDefault()
+    let form = document.getElementById("form__tag")
+    let inputbox1 = document.getElementById("task-input")
+    let inputbox2 = document.getElementById("due-date")
+    let inputbox3 = document.getElementById("select")
+    if (inputbox1.value === "" || inputbox2.value === "" || inputbox3.value === "") {
+        alert("Please fill out the form properly before submitting.")
+    }
+    else {
+        let noTask = document.getElementById("no__task")
         noTask.style.display = "none"
-        var div1 = document.createElement("div")
-        var div2 = document.createElement("div")
-        var div3 = document.createElement("div") 
-        var p1 = document.createElement("p")
-        var p2 = document.createElement("p")
-        var h1 = document.createElement("h1")
-        
+        let div1 = document.createElement("div")
+        let div2 = document.createElement("div")
+        let div3 = document.createElement("div")
+        let p1 = document.createElement("p")
+        let p2 = document.createElement("p")
+        let h1 = document.createElement("h1")
+
         div1.classList.add("inprogress__task")
         div2.classList.add("inprogress__category")
         div3.classList.add("inprogress__content")
@@ -44,7 +75,8 @@ closebtn.addEventListener("click",()=>{
         document.getElementById("inprogress__container").appendChild(div1)
 
         inputform.classList.add("hidden")
-        
-    } 
+
+    }
     form.reset()
-})
+});
+
